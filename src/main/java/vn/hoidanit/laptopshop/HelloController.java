fd4baf5f5d8a8f626dbf1bab8609a26755363a50
namdoanx03 <dpnbro1102@gmail.com>
2024-09-14 04:22:29 +0700

    first commit

--- src/main/java/vn/hoidanit/laptopshop/HelloController.java
+++ src/main/java/vn/hoidanit/laptopshop/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/")
-        public String index(){
-            return "Hello world from Nam doanx";
+        public String index() {
+            return "Hello world 2update???";
         }
 }
